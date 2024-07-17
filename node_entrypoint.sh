83d15ea4da4b2e4e0da9a3feb6560c25a349bfa8
James-Lu-none <jamesabcde277@gmail.com>
2024-07-17 21:22:56 +0800

    update script

--- node_entrypoint.sh
+++ node_entrypoint.sh
@@ -3,7 +3,7 @@
 # get join command
 # kubeadm token create $(kubeadm token generate) --print-join-command --ttl=0
 
-kubectl join [create] --cri-socket=unix:///var/run/cri-dockerd.sock
+# kubectl join [create] --cri-socket=unix:///var/run/cri-dockerd.sock
 
 #[ERROR FileAvailable--etc-kubernetes-kubelet.conf]: /etc/kubernetes/kubelet.conf already exists
 #[ERROR FileAvailable--etc-kubernetes-pki-ca.crt]: /etc/kubernetes/pki/ca.crt already exists
