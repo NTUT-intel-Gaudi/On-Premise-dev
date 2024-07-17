85092b7f4b6fb8a1feb103ac49d8da0d56c2edc4
James-Lu-none <jamesabcde277@gmail.com>
2024-07-17 22:14:36 +0800

    update node_entrypoint

--- node_entrypoint.sh
+++ node_entrypoint.sh
@@ -2,7 +2,14 @@
 
 # get join command
 # kubeadm token create $(kubeadm token generate) --print-join-command --ttl=0
+sudo swapoff -a
+sudo cat <<EOF | tee /etc/sysctl.d/k8s.conf
+net.ipv4.ip_forward = 1
+EOF
 
+sudo sysctl --system
+
+sudo kubeadm reset --cri-socket=unix:///var/run/cri-dockerd.sock
 # kubectl join [create] --cri-socket=unix:///var/run/cri-dockerd.sock
 
 #[ERROR FileAvailable--etc-kubernetes-kubelet.conf]: /etc/kubernetes/kubelet.conf already exists
@@ -10,3 +17,6 @@
 sudo rm /etc/kubernetes/kubelet.conf
 sudo rm /etc/kubernetes/pki/ca.crt
 sudo rm /etc/kubernetes/bootstrap-kubelet.conf
+
+kubectl apply -f calico.yaml
+kubectl get pods -A --watch
