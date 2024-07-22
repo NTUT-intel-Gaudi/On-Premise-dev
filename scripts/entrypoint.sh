234a6e5dd257dbd8e3d2135cd1e38136600a1283
James-Lu-none <jamesabcde277@gmail.com>
2024-07-22 23:04:53 +0800

    remove and update scripts

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,21 +1,33 @@
 #!bin/sh
-set -euo pipefail
 
 THIS_SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
 cd "$THIS_SCRIPT_PATH"
 
-swapoff -a
-cat <<EOF | tee /etc/sysctl.d/k8s.conf
+sudo swapoff -a
+sudo cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.ipv4.ip_forward = 1
 EOF
 
-sysctl --system
-kubeadm reset
-rm $HOME/.kube/config
-kubeadm init --pod-network-cidr=192.168.0.0/24 --cri-socket=unix:///var/run/cri-dockerd.sock
+sudo sysctl --system
 
-mkdir $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
+sudo kubeadm reset --cri-socket=unix:///var/run/cri-dockerd.sock --force
+sudo rm -rf /etc/cni/net.d
+sudo rm -rf /var/lib/cni/
+sudo rm $HOME/.kube/config
 
-# journalctl -u kubelet -f
+cd ../config
+sudo kubeadm init --config=kubeadm-config.yaml
+
+sudo mkdir $HOME/.kube/
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# apply network add on
+kubectl apply -f calico.yaml
+
+# apply storage class
+kubectl apply -f local-path-storage.yaml
+
+# apply 
+# kubectl create namespace argocd
+# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
