8b3d270180a09019060fdd930b4f05685a75210f
James-Lu-none <jamesabcde277@gmail.com>
2024-07-31 12:04:57 +0800

    chore: fix typo

--- scripts/kubeflow_entrypoint.sh
+++ scripts/kubeflow_entrypoint.sh
@@ -18,7 +18,7 @@ kustomize build common/istio-1-22/istio-install/overlays/oauth2-proxy | kubectl 
 
 echo "Waiting for all Istio Pods to become ready..."
 kubectl wait --for=condition=Ready pods --all -n istio-system --timeout=300s || \
-kubectl wait --for=condition=Complete pods --all -n istio-system --timeout=300s
+kubectl wait --for=condition=Completed pods --all -n istio-system --timeout=300s
 
 # install Oauth2-proxy
 echo "Installing oauth2-proxy..."
