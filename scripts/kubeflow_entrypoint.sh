4897e4028707128655efc9118022175cbca27ef5
James-Lu-none <jamesabcde277@gmail.com>
2024-07-31 11:48:48 +0800

    chore: Update entrypoint script and install dependencies using kustomize

--- scripts/kubeflow_entrypoint.sh
+++ scripts/kubeflow_entrypoint.sh
@@ -2,6 +2,8 @@
 
 cd /home/james/tools/manifests
 
+# kubeflow dependencies
+
 # install cert manager
 kustomize build common/cert-manager/cert-manager/base | kubectl apply -f -
 echo "Waiting for cert-manager to be ready ..."
@@ -34,3 +36,41 @@ kustomize build common/kubeflow-namespace/base | kubectl apply -f -
 
 # install Kubeflow Roles
 kustomize build common/kubeflow-roles/base | kubectl apply -f -
+
+# kubeflow application
+
+# install KServe
+kustomize build contrib/kserve/kserve | kubectl apply -f -
+kustomize build contrib/kserve/models-web-app/overlays/kubeflow | kubectl apply -f -
+
+# install Katib
+kustomize build apps/katib/upstream/installs/katib-with-kubeflow | kubectl apply -f -
+
+# install Central Dashboard
+kustomize build apps/centraldashboard/upstream/overlays/kserve | kubectl apply -f -
+
+# install Admission Webhook
+kustomize build apps/admission-webhook/upstream/overlays/cert-manager | kubectl apply -f -
+
+# install Notebook 1.0
+kustomize build apps/jupyter/notebook-controller/upstream/overlays/kubeflow | kubectl apply -f -
+kustomize build apps/jupyter/jupyter-web-app/upstream/overlays/istio | kubectl apply -f -
+
+# install PVC Viewer Controller
+kustomize build apps/pvcviewer-controller/upstream/default | kubectl apply -f -
+
+# install Profiles + KFAM
+kustomize build apps/profiles/upstream/overlays/kubeflow | kubectl apply -f -
+
+# install Volumes Web Application
+kustomize build apps/volumes-web-app/upstream/overlays/istio | kubectl apply -f -
+
+# install TensorBoard
+kustomize build apps/tensorboard/tensorboards-web-app/upstream/overlays/istio | kubectl apply -f -
+kustomize build apps/tensorboard/tensorboard-controller/upstream/overlays/kubeflow | kubectl apply -f -
+
+# install Training Operator
+kustomize build apps/training-operator/upstream/overlays/kubeflow | kubectl apply -f -
+
+# install Namespaces
+kustomize build common/user-namespace/base | kubectl apply -f -
