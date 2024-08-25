24acf7819922a481df11eba77fbd3f324b0292df
James-Lu-none <jamesabcde277@gmail.com>
2024-08-26 01:39:54 +0800

    update installation scripts

---
+++ scripts/ins.sh
@@ -0,0 +1,82 @@
+#!bin/sh
+set -euo pipefail
+
+cri=containerd
+os=debian
+arch=amd64
+
+THIS_SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
+cd "$THIS_SCRIPT_PATH"
+
+sudo apt update
+sudo apt upgrade
+sudo install -m 0755 -d /etc/apt/keyrings
+
+# install kubelet kubeadm kubectl
+# apt-transport-https may be a dummy package; if so, you can skip that package
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+sudo systemctl enable --now kubelet
+
+# install container interface
+sudo apt-get update
+sudo curl -fsSL "https://download.docker.com/linux/$os/gpg" -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] "https://download.docker.com/linux/$os" \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+if [ "$cri" = "containerd" ]; then
+  sudo apt-get install -y containerd.io
+  sudo mkdir -p /etc/containerd
+  containerd config default | sudo tee /etc/containerd/config.toml
+  # set SystemdCgroup to true for runc
+  sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+  sudo systemctl restart containerd
+  sudo systemctl enable containerd
+else if [ "$cri" = "docker" ]; then
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo systemctl enable --now docker
+
+  if [ "arch" = "amd64" ]; then
+    # install cri-dockerd
+    wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.3.15/cri-dockerd_0.3.15.3-0.debian-bookworm_amd64.deb
+    sudo dpkg -i cri-dockerd_0.3.15.3-0.debian-bookworm_amd64.deb
+    rm cri-dockerd_0.3.15.3-0.debian-bookworm_amd64.deb
+
+    systemctl daemon-reload
+    systemctl enable cri-docker.service
+    systemctl enable --now cri-docker.socket
+  else if [ "arch" = "arm64" ]; then
+    # install cri-dockerd
+    # https://alexsniffin.medium.com/a-guide-to-building-a-kubernetes-cluster-with-raspberry-pis-23fa4938d420
+  fi
+fi
+
+# install kustomize
+
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash
+sudo mv kustomize /usr/local/bin/
+
+# install nvidia container toolkit (runtime for docker)
+
+# curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+#   && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+# sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+# sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+# sed -i -e '/experimental/ s/^#//g' /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+# sudo apt-get update
+# sudo apt-get install -y nvidia-container-toolkit
+# sudo nvidia-ctk runtime configure --runtime=docker
+# sudo systemctl restart docker
