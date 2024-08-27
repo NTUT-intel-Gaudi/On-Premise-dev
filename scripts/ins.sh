9b962ec5365e17c333463a809d173c8c64f01575
James-Lu-none <jamesabcde277@gmail.com>
2024-08-27 13:30:30 +0800

    update installation script to install containerd from official binaries

--- scripts/ins.sh
+++ scripts/ins.sh
@@ -26,25 +26,36 @@ sudo apt-mark hold kubelet kubeadm kubectl
 sudo systemctl enable --now kubelet
 
 # install container interface
-sudo apt-get update
-sudo curl -fsSL "https://download.docker.com/linux/$os/gpg" -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] "https://download.docker.com/linux/$os" \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
 
 if [ "$cri" = "containerd" ]; then
-  sudo apt-get install -y containerd.io
-  sudo mkdir -p /etc/containerd
-  containerd config default | sudo tee /etc/containerd/config.toml
-  # set SystemdCgroup to true for runc
-  sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
-  sudo systemctl restart containerd
-  sudo systemctl enable containerd
+  if [ "arch" = "amd64" ]; then
+    # install containerd from the official binaries
+    wget https://github.com/containerd/containerd/releases/download/v1.7.21/containerd-1.7.21-linux-amd64.tar.gz
+    tar Cxzvf /usr/local containerd-1.7.21-linux-amd64.tar.gz
+    sudo mkdir -p /usr/local/lib/systemd/system
+    sudo wget -O /usr/local/lib/systemd/system/containerd.service https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
+    sudo systemctl daemon-reload
+    sudo systemctl enable --now containerd
+    # install runc
+    wget https://github.com/opencontainers/runc/releases/download/v1.1.13/runc.amd64
+    sudo install -m 755 runc.arm64 /usr/local/sbin/runc
+    #install cni plugin
+    wget https://github.com/containernetworking/plugins/releases/download/v1.5.1/cni-plugins-linux-amd64-v1.5.1.tgz
+    mkdir -p /opt/cni/bin
+    sudo tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.5.1.tgz
+  fi
+  
 elif [ "$cri" = "docker" ]; then
+  sudo apt-get update
+  sudo curl -fsSL "https://download.docker.com/linux/$os/gpg" -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] "https://download.docker.com/linux/$os" \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+
   sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
   sudo systemctl enable --now docker
 
