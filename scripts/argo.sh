9716c7b4fbfbecd9fdcabf8251d967d0ed85ae6e
James-Lu-none <jamesabcde277@gmail.com>
2024-08-29 01:31:40 +0800

    update argo script to install argocd

--- scripts/argo.sh
+++ scripts/argo.sh
@@ -6,5 +6,8 @@ cd "$THIS_SCRIPT_PATH"
 
 cd ./argocd-config
 
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
 kubectl apply -f kubeflow.yaml
-kubectl apply -f myapp.yaml
+# kubectl apply -f myapp.yaml
