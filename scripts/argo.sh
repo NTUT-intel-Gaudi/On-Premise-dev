0148139d781689c33234fd6d520a57d679d6e314
James-Lu-none <jamesabcde277@gmail.com>
2024-08-29 01:32:50 +0800

    resolve directory

--- scripts/argo.sh
+++ scripts/argo.sh
@@ -1,10 +1,9 @@
 #!bin/sh
-set -euo pipefail
 
 THIS_SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
 cd "$THIS_SCRIPT_PATH"
 
-cd ./argocd-config
+cd ../argocd-config
 
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
