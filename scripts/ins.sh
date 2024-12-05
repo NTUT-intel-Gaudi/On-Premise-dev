4b7b0698c0aff06324533322a3eca4cbb6e6ec19
James-Lu-none <jamesbacde277@gmail.com>
2024-12-06 01:02:30 +0800

    chore: Remove k8s installation steps

--- scripts/ins.sh
+++ scripts/ins.sh
@@ -1,33 +1,12 @@
 #!bin/sh
 set -euo pipefail
 
-k8s_version=v1.29
 cri=containerd
 os=debian
 arch=amd64
 
 THIS_SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
 cd "$THIS_SCRIPT_PATH"
-
-sudo apt update
-sudo apt upgrade
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo apt install net-tools jq apt-transport-https ca-certificates curl gpg
-
-# install systemd-resolved
-sudo apt install systemd-resolved
-sudo systemctl restart systemd-resolved
-sudo systemctl enable systemd-resolved
-
-# install kubelet kubeadm kubectl
-curl -fsSL "https://pkgs.k8s.io/core:/stable:/$k8s_version/deb/Release.key" | sudo gpg --dearmor -o "/etc/apt/keyrings/kubernetes-$k8s_version-apt-keyring.gpg"
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-$k8s_version-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$k8s_version/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
-
-sudo systemctl enable --now kubelet
 
 # install container interface
 
