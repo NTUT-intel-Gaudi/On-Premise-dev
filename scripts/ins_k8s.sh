3ac60aa333468c5da5d0b6ee95aa427c7e66825a
James-Lu-none <jamesbacde277@gmail.com>
2024-12-06 01:06:07 +0800

    chore: Update package installation commands to use -y flag for non-interactive installs

--- scripts/ins_k8s.sh
+++ scripts/ins_k8s.sh
@@ -8,10 +8,10 @@ fi
 k8s_version=$1
 read -p "installing k8s with version: $k8s_version..."
 
-sudo apt update
-sudo apt upgrade
+sudo apt update -y
+sudo apt upgrade -y
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo apt install net-tools jq apt-transport-https ca-certificates curl gpg
+sudo apt install -y net-tools jq apt-transport-https ca-certificates curl gpg
 
 # install systemd-resolved
 sudo apt install systemd-resolved
@@ -22,7 +22,7 @@ sudo systemctl enable systemd-resolved
 curl -fsSL "https://pkgs.k8s.io/core:/stable:/$k8s_version/deb/Release.key" | sudo gpg --dearmor -o "/etc/apt/keyrings/kubernetes-$k8s_version-apt-keyring.gpg"
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-$k8s_version-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$k8s_version/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
@@ -38,5 +38,5 @@ sudo mv kustomize /usr/local/bin/
 curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
 sudo apt-get install apt-transport-https --yes
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-sudo apt-get update
-sudo apt-get install helm
+sudo apt-get update -y
+sudo apt-get install helm -y
